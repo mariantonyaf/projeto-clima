547699b18b58eb092160ef77236febe0e928dbb5
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-06 12:53:28 -0300

    Update script.js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,40 +1,48 @@
 document.querySelector('#search').addEventListener('submit', async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const cityName = document.querySelector('#city_name').value
+    const cityName = document.querySelector('#city_name').value;
 
-    if(!cityName){
-        return showAlert('Você precisa digitar uma cidade...')
+    if (!cityName) {
+        document.querySelector("#weather").classList.remove('show');
+        showAlert('Você precisa digitar uma cidade...');
+        return;
     }
 
-    const apiKey = 'b1fb29e623b32e54cc7a4f9b877e0bf6'
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid=${apiKey}&units=metric&lang=pt-br`
+    const apiKey = '8a60b2de14f7a17c7a11706b2cfcd87c';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid=${apiKey}&units=metric&lang=pt_br`
 
-    const results = await fetch(apiUrl)
-    const json = await results.json()
+    const results = await fetch(apiUrl);
+    const json = await results.json();
 
-    if(json.cod === 200){
+    if (json.cod === 200) {
         showInfo({
             city: json.name,
             country: json.sys.country,
             temp: json.main.temp,
             tempMax: json.main.temp_max,
             tempMin: json.main.temp_min,
-            description: json.weather[0],description,
-            tempIcon: json.weather[0],icon,
+            description: json.weather[0].description,
+            tempIcon: json.weather[0].icon,
             windSpeed: json.wind.speed,
-            humidity: json.main.humidity
-        })
-    } else{
-        document.querySelector("#weather").classList.remove('show')
-        showAlert('Não foi possível localizar...')
+            humidity: json.main.humidity,
+        });
+    } else {
+        document.querySelector("#weather").classList.remove('show');
+        showAlert(`
+            Não foi possível localizar...
+
+            <img src="src/images/404.svg"/>
+        `)
     }
-})
+});
 
 function showInfo(json){
-    showAlert('')
+    showAlert('');
 
-    document.querySelector("#weather").classList.add('show')
+    document.querySelector("#weather").classList.add('show');
+
+    document.querySelector('#title').innerHTML = `${json.city}, ${json.country}`;
 
     document.querySelector('#temp_value').innerHTML = `${json.temp.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
     document.querySelector('#temp_description').innerHTML = `${json.description}`;
@@ -46,6 +54,6 @@ function showInfo(json){
     document.querySelector('#wind').innerHTML = `${json.windSpeed.toFixed(1)}km/h`;
 }
 
-function showAlert(msg){
-    document.querySelector('#alert').innerHTML = msg
+function showAlert(msg) {
+    document.querySelector('#alert').innerHTML = msg;
 }
